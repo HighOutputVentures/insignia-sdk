b9cc6f920cf9ce2421b7798d91763375e0809376
iammeosjin <iammeosjin@gmail.com>
2021-05-04 18:47:53 +0800

    feat: add handshake query in websocket

--- src/event/listen.ts
+++ src/event/listen.ts
@@ -1,11 +1,6 @@
 import { EventEmitter } from 'events';
-import io from 'socket.io-client';
-import config from 'src/library/config';
 import { UserEvent, UserEventType } from 'src/type';
 
-const socket = io(`${config.host}/socket`, {
-  reconnectionDelayMax: 10000,
-});
 class CustomEventEmitter extends EventEmitter {
   on(input: 'data', cb: (data: UserEvent) => void) {
     super.on(input, cb);
@@ -15,8 +10,11 @@ class CustomEventEmitter extends EventEmitter {
 
 const userEvents = new CustomEventEmitter();
 
-export default function listenEvents(appId: string, type?: UserEventType) {
-  socket.on(`${appId}:events`, (message: any) => {
+export default function listenEvents(
+  options: { appId: string; socket: SocketIOClient.Socket },
+  type?: UserEventType,
+) {
+  options.socket.on(`${options.appId}:events`, (message: any) => {
     const event: UserEvent = JSON.parse(message);
 
     if (type && type !== event.type) {
--- src/server-client.ts
+++ src/server-client.ts
@@ -1,25 +1,57 @@
 import { ID } from 'src/type';
+import R from 'ramda';
+import io from 'socket.io-client';
 import createUser from 'src/user/create';
 import updateUser from 'src/user/update';
 import deleteUser from 'src/user/delete';
 import listenEvents from 'src/event/listen';
 import fetchEvents from 'src/event/fetch';
+import config from './library/config';
 
 export default class ServerClient {
-  private opts: { appId: string; appKey: string; host?: string };
+  private opts: {
+    appId: string;
+    appKey: string;
+    host?: string;
+    socket: SocketIOClient.Socket;
+  };
 
   public constructor(opts: { appId: string; appKey: string; host?: string }) {
-    this.opts = opts;
+    this.opts = {
+      ...opts,
+      socket: io(`${opts.host || config.host}`, {
+        reconnectionDelayMax: 10000,
+        path: '/events',
+        query: {
+          appId: opts.appId,
+          appKey: opts.appKey,
+        },
+      }),
+    };
   }
 
   public get user() {
     const client = this as ServerClient;
     return {
       create: (input: Parameters<typeof createUser>[2]) =>
-        createUser(client.opts.host, client.opts, input),
+        createUser(
+          client.opts.host,
+          R.pick(['appId', 'appKey'])(client.opts),
+          input,
+        ),
       update: (id: ID, input: Parameters<typeof updateUser>[3]) =>
-        updateUser(client.opts.host, client.opts, id, input),
-      delete: (id: ID) => deleteUser(client.opts.host, client.opts, id),
+        updateUser(
+          client.opts.host,
+          R.pick(['appId', 'appKey'])(client.opts),
+          id,
+          input,
+        ),
+      delete: (id: ID) =>
+        deleteUser(
+          client.opts.host,
+          R.pick(['appId', 'appKey'])(client.opts),
+          id,
+        ),
     };
   }
 
@@ -27,11 +59,18 @@ export default class ServerClient {
     const client = this as ServerClient;
     return {
       listen: ((input: Parameters<typeof listenEvents>[1]) =>
-        listenEvents(client.opts.appId, input)) as (
+        listenEvents(
+          { appId: client.opts.appId, socket: client.opts.socket },
+          input,
+        )) as (
         type: Parameters<typeof listenEvents>[1],
       ) => ReturnType<typeof listenEvents>,
       fetch: (input: Parameters<typeof fetchEvents>[2]) =>
-        fetchEvents(client.opts.host, client.opts, input),
+        fetchEvents(
+          client.opts.host,
+          R.pick(['appId', 'appKey'])(client.opts),
+          input,
+        ),
     };
   }
 }
