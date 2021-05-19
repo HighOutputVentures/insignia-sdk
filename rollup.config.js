b681fcdbecd2fb8b77d0bde5224b4999d97414fb
iammeosjin <iammeosjin@gmail.com>
2021-05-19 09:30:55 +0800

    build: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,31 +3,33 @@ import { uglify } from 'rollup-plugin-uglify';
 import typescript from 'rollup-plugin-typescript2';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from '@rollup/plugin-json';
 import tsc from 'typescript';
 
 const config = {
-  input: 'src/web-client/browser/index.ts',
+  input: 'src/browser/index.ts',
   output: {
     format: process.env.ROLLUP_FORMAT,
     exports: 'named',
     name: 'insignia',
     globals: {
       fetch: 'fetch',
+      'url-parse': 'Url',
     },
   },
+  external: ['url-parse'],
   plugins: [
     json(),
     typescript({
       typescript: tsc,
       tsconfig: 'tsconfig.rollup.json',
     }),
+    builtins(),
+    globals(),
     resolve({ preferBuiltins: true, browser: true }),
     commonjs(),
-    globals(),
-    builtins({ crypto: true }),
   ],
 };
 
